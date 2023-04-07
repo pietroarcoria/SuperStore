b0300ab45a9388cc6e0bdb5b17223a6e99f139fa
pietroarcoria <114586665+pietroarcoria@users.noreply.github.com>
2023-04-07 17:18:38 -0400

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,40 @@
+public class Item {
+
+    int id;
+    String name;
+    int price;
+    String category;
+
+
+    public Item(int getId, String getName, int getPrice, String getCategory) {
+      
+        id = getId();
+        name = getName();
+        price = getPrice();
+        category = getCategory();
+        
+        
+
+    }
+
+    String getCategory() {
+        return null;
+    }
+
+    int getPrice() {
+        return 0;
+    }
+
+    etName() {
+        return null;
+    }
+
+    public int getId() {
+        return ;
+    }
+
+    public String getName() {
+        return null;
+    }
+    
+}

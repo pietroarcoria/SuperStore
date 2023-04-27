478bac163272204144b010d0d688b0b4faa633d8
pietroarcoria <114586665+pietroarcoria@users.noreply.github.com>
2023-04-27 09:01:22 -0400

    Update Item.java

--- Item.java
+++ Item.java
@@ -25,16 +25,56 @@ public class Item {
         return 0;
     }
 
-    etName() {
+    Object getItem() {
         return null;
     }
 
     public int getId() {
-        return ;
+        return id ;
     }
 
     public String getName() {
         return null;
     }
-    
+}
+
+class Supercar extends Item {
+
+    public Supercar(int id, String name, int price) {
+        super(id, name, price, "Supercar");
+    }
+
+    @Override
+    public String getCategory() {
+        return "Supercar";
+    }
+}
+
+
+
+class SUV extends Item {
+
+    public SUV(int id, String name, int price) {
+        super(id, name, price, "SUV");
+    }
+
+    @Override
+    public String getCategory() {
+        return "SUV";
+    }
+}
+
+
+
+
+    class Drift extends Item {
+
+    public Drift(int id, String name, int price) {
+        super(id, name, price, "Drift");
+    }
+
+    @Override
+    public String getCategory() {
+        return "Drift";
+    }
 }
